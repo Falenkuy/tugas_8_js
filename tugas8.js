ca9a5805c1a986b6243841c51f2d85f1737d55d1
Valencinus Aten <valenaten22@gmail.com>
2020-10-06 22:18:26 +0700

    Tugas 8 js
    Tugas ini saya sampaikan ke tim Niomic

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function ferrari(){
+  var mobil =  {
+    type:'sedan',
+    warna:'merah',
+    harga:2000000000000,
+    tahun:[20,30,40,60,60]
+  }
+  mobil.warna = "biru";
+  console.log(mobil)
+}
+ferrari()
